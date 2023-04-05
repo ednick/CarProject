a4d0fae5ce9c3d36a93afa97b706d47778f82704
Tysef Foster <ftysef@gmail.com>
2023-04-05 14:37:26 -0400

    changes on configureService, addRepository and ICarRepository Interface

---
+++ CarProject.DataAccess/ConfigureServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarProject.DataAccess.Repository;
+using CarProject.Domain;
+using SimpleInjector;
+
+namespace CarProject.DataAccess
+{
+    public static class ConfigureServices
+    {
+        public static Container AddRepositoryServices(this Container services)
+        {
+            services.AddDomainServices();
+            services.Register<ICarRepository, CarRepository>();
+            return services;
+        }
+    }
+}
---
+++ CarProject.DataAccess/Repository/CarRepository.cs
@@ -0,0 +1,70 @@
+﻿using CarProject.Domain;
+using CarProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject.DataAccess.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+        private readonly List<Car> _cars;
+
+        public CarRepository()
+        {
+            _cars = new List<Car>();
+        }
+
+        public  Task InsertCarAsync(Car car)
+        {
+            using (var s = new CarContext())
+            {
+                s.Cars.Add(car);
+                s.SaveChanges();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public async Task UpdateCarAsync(Car car)
+        {
+            using (var s = new CarContext())
+            {
+                var existingCar = s.Cars.FirstOrDefault(c => c.Id == car.Id);
+                if (existingCar != null)
+                {
+                    existingCar.Name = car.Name;
+                    existingCar.Color = car.Color;
+                    existingCar.YearMade = car.YearMade;
+                    await s.SaveChangesAsync();
+                }
+            }
+            
+        }
+
+        public async Task<List<Car>> GetAllCarsAsync()
+        {
+            using (var s = new CarContext())
+            {
+                var carList =  s.Cars.ToList();
+
+                return carList;
+            }
+             
+        }
+
+        public async Task<Car> GetCarByIdAsync(int id)
+        {
+            using (var s = new CarContext())
+            {
+                var result = await s.Cars.FirstOrDefaultAsync(c => c.Id == id);
+
+                return result;
+            }
+        }
+    }
+
+}
