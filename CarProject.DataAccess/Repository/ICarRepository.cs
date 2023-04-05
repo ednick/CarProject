3ae966195b771b0a56a6c92371cfae861817f8c3
Tysef Foster <ftysef@gmail.com>
2023-04-05 15:22:20 -0400

    add UnitTest Project

--- CarProject.DataAccess/Repository/ICarRepository.cs
+++ CarProject.DataAccess/Repository/ICarRepository.cs
@@ -6,9 +6,31 @@ namespace CarProject.DataAccess.Repository
 {
     public interface ICarRepository
     {
+        /// <summary>
+        /// Get All Cars
+        /// </summary>
+        /// <returns></returns>
         Task<List<Car>> GetAllCarsAsync();
+        
+        /// <summary>
+        /// Get Cars By ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<Car> GetCarByIdAsync(int id);
+        
+        /// <summary>
+        /// Insert/ Create new Car
+        /// </summary>
+        /// <param name="car"></param>
+        /// <returns></returns>
         Task InsertCarAsync(Car car);
+
+        /// <summary>
+        /// Update Car Information
+        /// </summary>
+        /// <param name="car"></param>
+        /// <returns></returns>
         Task UpdateCarAsync(Car car);
     }
 }
